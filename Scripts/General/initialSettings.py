3516a73469fbbee32ae13aa5621d1e0f263acf1d
Neal <info@nealbuerger.com>
2021-03-30 12:48:10 +0200

    prep for Maya 2022

---
+++ Scripts/General/initialSettings.py
@@ -0,0 +1,23 @@
+import pymel.core as pm
+import maya.cmds as cmds
+
+
+def set_initial_settings():
+    # Grid Settings
+    pm.grid(size=50, displayPerspectiveLabels=True)
+    pm.grid(default=True, size=50, displayPerspectiveLabels=True)
+
+    # Enable Autosave
+    pm.system.autoSave(enable=True, limitBackups=True, maxBackups=15)
+
+    # Enable Infinite Undo
+    pm.system.undoInfo(infinity=True)
+
+    # Enable Poly Face Select with Center
+    pm.modeling.polySelectConstraint(wholeSensitive=False)
+
+
+if __name__ == "__main__":
+    if pm.versions.current() >= 20180000:
+        print("Run Initial Settings")
+        set_initial_settings()
